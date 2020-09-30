b35a000babe7bc95e1d3ab487be6c6be944dd0d3
Josh Ford <thisisjoshford@gmail.com>
2020-09-30 15:41:36 -0700

    updated instructions

--- send-tokens.js
+++ send-tokens.js
@@ -5,6 +5,7 @@ const { transactions, utils } = api;
 //configure your network
 const near = setupNear('testnet');
 
+//configure accounts and amount of NEAR to send
 const sender = 'nearkat.testnet';
 const receiver = 'joshford.testnet';
 const amount = formatAmount(1);
@@ -13,15 +14,15 @@ const networkId = near.config.networkId;
 async function main() {
   console.log('Processing transaction...')
 
-  // get public key for sending account
+  // get sender's public key
   const publicKey = await near.connection.signer.getPublicKey(sender, networkId);
 
-  // throw error to console if publicKey is not found in ~/.near-credentials
+  // throw error to console if publicKey is not found in /HOME/.near-credentials
   if(!publicKey || publicKey === null) {
     return console.log(`ERROR: Key not found for sender account: [ ${sender} ]`);
   };
 
-  //gets key information from provided signer public key
+  //gets key information from blockchain using sender's public key
   const accessKey = await near.connection.provider.query(`access_key/${sender}/${publicKey.toString()}`, '');
 
   // return error to console if key is not a full access key
@@ -30,10 +31,10 @@ async function main() {
     };
 
   // gets current nonce from the key and adds 1 to create a nonce for this transaction
-  // each time the key is used in a transaction the nonce is incremented by 1
+  // each time the key is used in a transaction, the nonce is incremented by 1
   const nonce = ++accessKey.nonce;
 
-  // constructs an actions object that will be passed to the createTransaction method below
+  // constructs actions that will be passed to the createTransaction method below
   const actions = [transactions.transfer(amount)];
   
   // create transaction
@@ -46,7 +47,7 @@ async function main() {
     utils.serialize.base_decode(accessKey.block_hash)
     );
   // **NOTE** the last argument passed is a current block hash we received when declaring 
-  // the accessKey variable on line 25. We pass this particular argument to prove that 
+  // the accessKey variable on line 26. We pass this particular argument to prove that 
   // the transaction was constructed recently. (within last 12 hrs)
 
   // sign transaction
@@ -61,7 +62,7 @@ async function main() {
   try {
     //encodes transaction to serialized BORSH (required for all transactions)
     const bytes = signedTx.encode();
-    //sends transaction to NEAR via JSON RPC call and records the result
+    //sends transaction to NEAR blockchain via JSON RPC call and records the result
     const result = await near.connection.provider.sendJsonRpc(
       'broadcast_tx_commit', 
       [Buffer.from(bytes).toString('base64')]
